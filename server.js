629473aa055a18a602748f3dd8b8c5cb9533d008
krushnachavan30 <krushnachavan765@gmail.com>
2026-06-18 08:30:13 -0700

    update

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.use('/api/admin', adminRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err))
+app.get("/",(req,res)=>{
+  res.send({
+    msg:"res form render"
+  })
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
